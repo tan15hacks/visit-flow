cba85ede594ec72d2e87da296b2d19aad493a8da
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-10 14:05:45 +0800

    feat(auth): expose session controller through Riverpod

---
+++ apps/staff_mobile/lib/features/authentication/presentation/auth_providers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:visitflow_staff/features/authentication/presentation/auth_session_controller.dart';
+
+final authSessionControllerProvider = Provider<AuthSessionController>((ref) {
+  throw StateError('AuthSessionController must be overridden during bootstrap.');
+});
